3a9708ce357255976f3b61b6d0338696426ddafb
katarzyna_w <katarzyna.koch@gmail.com>
2021-06-16 15:22:49 -0500

    fetch API JSON data

--- js/script.js
+++ js/script.js
@@ -2,3 +2,12 @@
 const profileInfo = document.querySelector(".overview");
 const username = "katarzyna-kw";
 
+const fetchFromProfile = async function () {
+    const response = await fetch (
+        `https://api.github.com/users/${username}`
+    )
+    const data = await response.json();
+    console.log("data: ", data)
+}
+
+fetchFromProfile();
