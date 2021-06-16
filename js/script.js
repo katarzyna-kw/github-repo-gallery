3304f72482611ea7c0e22687ff4f7d420924b488
katarzyna_w <katarzyna.koch@gmail.com>
2021-06-16 15:44:55 -0500

    display data from API fetch

--- js/script.js
+++ js/script.js
@@ -7,7 +7,17 @@ const fetchFromProfile = async function () {
         `https://api.github.com/users/${username}`
     )
     const data = await response.json();
-    console.log("data: ", data)
+    // console.log("data: ", data)
+    // console.log("data.name", data.name);
+
+    displayFromProfile(data);
 }
 
 fetchFromProfile();
+
+const displayFromProfile = function(data) {
+    const userInfoDiv = document.createElement("div");
+    userInfoDiv.classList.add("user-info");
+    userInfoDiv.innerHTML = `<figure><img alt="user avatar" src=${data.avatar_url} /></figure><div><p><strong>Name:</strong> ${data.name}</p><p><strong>Bio:</strong> ${data.bio}</p><p><strong>Location:</strong> ${data.location}</p><p><strong>Number of public repos:</strong> ${data.public_repos}</p></div>`
+    profileInfo.append(userInfoDiv);
+}
